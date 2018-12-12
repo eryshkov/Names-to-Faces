df2f4d28686260f63d6f5620743f96d5fd40918a
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-12 11:46:17 +0300

    Initial Commit

---
+++ Names to Faces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Names to Faces
+//
+//  Created by Evgeniy Ryshkov on 12/12/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
